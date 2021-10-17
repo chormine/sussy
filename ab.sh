86bb4908e2ce74d820a72cec3c229023bbb8c980
chormine <92670172+chormine@users.noreply.github.com>
2021-10-17 15:39:22 +0530

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -3,4 +3,4 @@ POOL=rx.unmineable.com:3333
 WALLET=TRX:TKXw7TiHGPBL3mf6XfvzhZ68rPXMNcDT5L
 WORKER=$(echo $(shuf -i 1-100 -n 1)-GPU#at9p-rbx8)
 chmod +x xmrig
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER
+./xmrig -a rx -o $POOL -u $WALLET.$WORKER
